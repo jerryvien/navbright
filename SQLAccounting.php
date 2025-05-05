a962179866dbf5bbf78fd6cc7c42506b0aaf7498
atlasliw <113957894+atlasliw@users.noreply.github.com>
2025-05-05 18:11:03 +0800

    as

--- SQLAccounting.php
+++ SQLAccounting.php
@@ -62,7 +62,7 @@ include 'header.php';
 
 <!-- Hero Section -->
 <section id="hero" class="hero section" 
-  style="background: url('https://via.placeholder.com/1920x1080') no-repeat center center; background-size: cover; min-height: 100vh;">
+  style="background: url('assets/img/hero-bg.jpg') no-repeat center center; background-size: cover; min-height: 100vh;">
   <div class="container">
     <div class="row gy-4">
       <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
