48f7bb4c3fb4bf1dff380c5837a3c19aaceff3d9
atlasliw <113957894+atlasliw@users.noreply.github.com>
2025-05-05 23:04:52 +0800

    as

--- SQLAccounting.php
+++ SQLAccounting.php
@@ -9,9 +9,9 @@ include 'header.php';
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Navbright Technology - SQL Accounting Solutions</title>
-  <meta name="description" content="Navbright Technology, an authorized SQL Accounting dealer in Malaysia, offers cloud-based accounting software to streamline invoicing, inventory, and financial reporting for businesses. Discover our tailored implementation, customization, and support services.">
-  <meta name="keywords" content="SQL Accounting, Accounting Software, Navbright Technology, E-Invoicing, Financial Reporting, Inventory Management, IT Solutions, Malaysia">
+  <title>SQL Accounting | Navbright Technology</title>
+  <meta name="description" content="Navbright Technology offers cloud-based SQL Accounting software in Malaysia to streamline invoicing, inventory, and financial reporting.">
+  <!-- Removed deprecated meta keywords tag -->
 
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
@@ -20,7 +20,7 @@ include 'header.php';
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
