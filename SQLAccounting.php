d1b590cfdc3bf43f326600e76ea923b1812a0871
atlasliw <113957894+atlasliw@users.noreply.github.com>
2025-05-06 14:36:42 +0800

    sd

--- SQLAccounting.php
+++ SQLAccounting.php
@@ -122,15 +122,12 @@ include 'header.php';
       <div class="col-lg-3 col-md-6">
         <div class="pricing-card h-100 shadow-sm p-4 bg-white rounded">
           <h3 class="text-primary">SQL Accountant Set</h3>
-          <p class="price display-7">RM1,899</p>
+          <p class="price display-7">RM1,899</p></p>
           <ul class="list-unstyled mt-3 mb-4">
-          <p></p>
-          <p></p>
           <li><i class="bi bi-check-circle-fill text-success me-2"></i> 1 Company</li>
           <li><i class="bi bi-check-circle-fill text-success me-2"></i> 1 Concurent User</li>
           <li><i class="bi bi-check-circle-fill text-success me-2"></i> 1 Session Training</li>
           <li><i class="bi bi-check-circle-fill text-success me-2"></i> 1st Year Support</li>
-          <p></p>
           <p></p>
             <li><i class="bi bi-check-circle-fill text-success me-2"></i> General Ledger</li>
             <li><i class="bi bi-check-circle-fill text-success me-2"></i> Customer</li>
