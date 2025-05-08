9716bc9e81d3c325e2ed3566b9cd1f2a97bb39f8
atlasliw <113957894+atlasliw@users.noreply.github.com>
2025-05-08 14:22:19 +0800

    asd

--- SQLAccounting.php
+++ SQLAccounting.php
@@ -74,6 +74,7 @@ include 'header.php';
         <div class="d-flex flex-column flex-md-row" data-aos="fade-up" data-aos-delay="200">
         <p></p>
         <p></p>
+        <p></p>
           <a href="#accountant-pricing" class="btn-get-started">Price <i class="bi bi-arrow-right"></i></a>
         </div>
       </div> 
