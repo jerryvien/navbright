2dcb562ae9195230295f06d6e28fba148c6f9fcf
atlasliw <113957894+atlasliw@users.noreply.github.com>
2025-05-08 13:40:09 +0800

    sad

--- SQLAccounting.php
+++ SQLAccounting.php
@@ -123,6 +123,8 @@ include 'header.php';
 
           <h3 class="text-primary">SQL Accountant Set</h3>
           <p class="price display-7">RM1,899</p>
+          <p></p>
+          <p></p>
           <ul class="list-unstyled mt-3 mb-4">
             <li><i class="bi bi-check-circle-fill text-success me-2"></i> 1 Company</li>
             <li><i class="bi bi-check-circle-fill text-success me-2"></i> 1 Concurent User</li>
