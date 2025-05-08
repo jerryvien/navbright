e647556cb2e862075ed63bf4e83ac7f282e0d2c4
atlasliw <113957894+atlasliw@users.noreply.github.com>
2025-05-08 14:09:15 +0800

    sad

--- footer.php
+++ footer.php
@@ -96,6 +96,3 @@
 </body>
 </html>
 
-<a class="btn-getstarted" href="https://api.whatsapp.com/send?phone=60179726386&text=Hello,%20I%20would%20like%20to%20inquire%20about%20your%20services." target="_blank">
-        <i class="fab fa-whatsapp"></i> WhatsApp Us
-      </a>
